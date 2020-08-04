7fa7a7ee3d56e106a69429b5f3fc18b9893e0eba
mehrad_adler@yahoo.com <mehrad_adler@yahoo.com>
2020-08-04 17:47:39 +0430

    set TableLayout and color for chaange tasks

--- app/src/main/java/com/example/recycleviewlist/WorkListActivityNotFragment.java
+++ app/src/main/java/com/example/recycleviewlist/WorkListActivityNotFragment.java
@@ -1,8 +1,11 @@
 package com.example.recycleviewlist;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -20,14 +23,56 @@ public class WorkListActivityNotFragment extends AppCompatActivity {
 
     ViewPager2 mViewPagerTask;
     List<State> mStates = new ArrayList<>(Arrays.asList(State.DONE, State.DOING, State.TODO));
+    TextView mTextViewDone;
+    TextView mTextViewToDo;
+    TextView mTextViewDoing;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_list_not_fragment);
+
+        mTextViewDoing = findViewById(R.id.textView_doing);
+        mTextViewDone = findViewById(R.id.textView_done);
+        mTextViewToDo = findViewById(R.id.textView_todo);
+
         mViewPagerTask = findViewById(R.id.ViewPager_task);
         mViewPagerTask.setAdapter(new ViewPagerAdapter(this));
+        mViewPagerTask.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                SetTableColors(position);
+            }
 
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+
+    }
+
+    private void SetTableColors(int position) {
+        mTextViewToDo.setBackgroundResource(R.color.color_bachground_table_of);
+        mTextViewDone.setBackgroundResource(R.color.color_bachground_table_of);
+        mTextViewDoing.setBackgroundResource(R.color.color_bachground_table_of);
+        switch (position) {
+            case 0:
+                mTextViewDone.setBackgroundResource(R.color.color_bachground_table_on);
+                break;
+            case 1:
+                mTextViewDoing.setBackgroundResource(R.color.color_bachground_table_on);
+                break;
+            case 2:
+                mTextViewToDo.setBackgroundResource(R.color.color_bachground_table_on);
+                break;
+        }
     }
 
     private class ViewPagerAdapter extends FragmentStateAdapter {
