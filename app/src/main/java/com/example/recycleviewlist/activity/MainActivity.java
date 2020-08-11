a5940cab0e01937342d37ba594b81f0a2d84f459
mehrad_adler@yahoo.com <mehrad_adler@yahoo.com>
2020-08-11 17:43:08 +0430

    HW12 all done

--- app/src/main/java/com/example/recycleviewlist/activity/MainActivity.java
+++ app/src/main/java/com/example/recycleviewlist/activity/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_work_list);
+        setContentView(R.layout.activity_work_view_pager);
         setFragments();
         findView();
         setOnClick();
